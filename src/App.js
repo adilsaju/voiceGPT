7e8e66bd56fd0b299d06e1e91f282a7f9dae06bb
Adil Saju <adilsaju@gmail.com>
2023-02-04 04:52:31 -0800

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [isListening, setisListening] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="hed">
+
+        <h1>voiceGPT</h1>
+        <div className='adil'>
+
+          <input type="text" id="text" />
+          {/* <input type="button" id="voice" value="&#127897;" onClick={()=>{setisListening(true)}}/> */}
+
+          { !isListening ?  <input type="button" id="voice" value="&#127897;" onClick={()=>{setisListening(true)}}/> :  <input type="button" id="voice" value="&#128721;" onClick={()=>{setisListening(false)}}/>  }
+
+
+        </div>
+        
+        {isListening ? <div style={{fontSize: ".9rem" }} id="notification">Listening</div>  :  <div style={{fontSize: ".9rem" }} id="notification">Not Listening</div>} 
+         
+        
+         
+         
+
+        </div>
+
+
+        <div className="out">
+          {/* <p>blahh</p>
+          <p>blahhhhh</p>
+          <p>adil</p>
+          <p>adil</p>
+          <p>adil</p>
+          <p>adil</p>
+          <p>adil</p>
+          <p>adil</p> */}
+
+        </div>
+        </header>
+          <footer>&copy; Adil Saju</footer>
     </div>
   );
 }
